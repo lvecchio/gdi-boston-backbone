97535524ac564ca41206deaddc25ad2ee7d03e2d
Laura Vecchio <laura.m.vecchio@gmail.com>
2015-02-21 16:20:54 -0500

    Creating a teacher model

---
+++ src/js/models/teacherModel.js
@@ -0,0 +1,10 @@
+// namespace
+var app = app || {};
+
+// teacher model
+app.teacher = Backbone.Model.extend({
+    // model defaults
+    defaults: {
+        name: 'GDI Boston Teacher'
+    }
+});
