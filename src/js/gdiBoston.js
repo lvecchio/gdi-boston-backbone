402f4652238a4cfc2f30bf42e8ac9fc9ca36b156
Laura Vecchio <laura.m.vecchio@gmail.com>
2015-02-21 16:34:53 -0500

    Creating two instances of users and initializing the user model.

---
+++ src/js/gdiBoston.js
@@ -0,0 +1,9 @@
+var user1 = new app.user({
+    name: 'Laura Vecchio',
+    description: 'Chapter Leader & Teacher'
+})
+
+var user2 = new app.user({
+    name: 'Liz Long',
+    description: 'Chapter Leader'
+})
--- src/js/models/userModel.js
+++ src/js/models/userModel.js
@@ -3,8 +3,14 @@ var app = app || {};
 
 // teacher model
 app.user = Backbone.Model.extend({
+
     // model defaults
     defaults: {
         name: 'GDI Boston Leader'
+    },
+
+    // initialize function
+    initialize: function () {
+        console.log('A model instance named ' + this.get('name') + ' has been created.');
     }
 });
